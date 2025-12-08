5b41eb3334347d89757bdce01c632eb26f2d9dc5
Martin R <martin.ristvedt@gmail.com>
2025-12-08 15:03:14 +0100

    endret unlockLuke så den ser etter id istedenfor klasse (createId), videreutviklet litt styling på kalender, brukte openable og la til et par funksjoner (updateOpenables, closeAllLukes)

--- src/components/KalenderComponent.vue
+++ src/components/KalenderComponent.vue
@@ -1,16 +1,20 @@
 <script lang="ts" setup>
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
+import { useId } from 'vue';
 import { useFirebaseStore } from '@/stores/firebaseStore';
 const firebaseStore = useFirebaseStore();
 const currentDay = firebaseStore.currentDate.toDate().getDate();
 import type { CalenderDay } from '@/stores/firebaseStore';
+import firebase from 'firebase/compat/app';
 
 const unlockLuke = (dayObj: CalenderDay, dayNum: number) => {
-  const getCurrentElement = document.getElementsByClassName("dag")[dayNum - 1];
+  const getCurrentElement = document.getElementById(`luke_${dayNum}`);
   const getCurrentElementbutton = getCurrentElement?.getElementsByTagName("button")[0];
   // dayObj.openable !!!!
 
-  if (dayNum <= currentDay) { //<-- dette burde være dayObj.openable!!
+  updateOpenables();
+
+  if (dayObj.openable == true) { //<-- dette burde være dayObj.openable!!
     if (dayObj.opened == true) {
       getCurrentElementbutton!.innerHTML = "åpne";
       //buttonTxt.value = "Open";
@@ -22,26 +26,52 @@ const unlockLuke = (dayObj: CalenderDay, dayNum: number) => {
     }
   } else {
     getCurrentElement!.getElementsByTagName("div")[0]!.innerHTML += `<div>Å NO YOU DONT!!</div>`;
-
   }
 }
+
+const updateOpenables = () => {
+  for (const key in firebaseStore.julekalender){
+    const item = firebaseStore.julekalender[key as keyof typeof firebaseStore.julekalender];
+    if (item.day <= currentDay) item.openable = true;
+    console.log(item.openable);
+  }
+}
+
+const closeAllLukes = () => {
+  let i = 0;
+  for (const key in firebaseStore.julekalender){
+    const item = firebaseStore.julekalender[key as keyof typeof firebaseStore.julekalender];
+    if (item.opened == true) item.opened = false;
+  }
+}
+
+const createId = (dagtall: number) => {
+  return `luke_${dagtall}`;
+}
+
+/*onMounted(() => {
+  updateOpenables();
+})*/
 </script>
 <template>
+  <div>
+    <button @click="closeAllLukes" style="padding: 30px;">Lukk alle</button>
+  </div>
   <div class="kalender">
 
-    <div class="dag" v-for="(day, index) in firebaseStore.julekalender" :key="index">
-      <h3>Dag {{ day.day }}</h3>
-
-      <button @click="unlockLuke(day, day.day)">lås opp</button> <!--:disabled="day.day > currentDay"--->
-      <div v-show="day.opened">
-        <p>{{ day.texts }}</p>
-        <img :src="day.image" alt="Bilde for dagen" v-if="day.image" />
-        <div v-if="day.video">
-          <iframe :src="day.video"></iframe>
+    <div v-for="(day, index) in firebaseStore.julekalender" :key="index">
+      <div :id="createId(day.day)" class="dag" :class="{ heightChange: day.opened }">
+        <h3>Dag {{ day.day }}</h3>
+        <button @click="unlockLuke(day, day.day)">lås opp</button> <!--:disabled="day.day > currentDay"--->
+        <div v-show="day.opened">
+          <p>{{ day.texts }}</p>
+          <img :src="day.image" alt="Bilde for dagen" v-if="day.image" />
+          <div v-if="day.video">
+            <iframe :src="day.video"></iframe>
+          </div>
         </div>
       </div>
     </div>
-
   </div>
 </template>
 
@@ -49,19 +79,31 @@ const unlockLuke = (dayObj: CalenderDay, dayNum: number) => {
 .kalender {
   display: grid;
   max-width: 1000px;
-   grid-template-columns: repeat(auto-fit, minmax(140px, 1fr));
+  grid-template-columns: repeat(auto-fit, minmax(140px, 1fr));
   gap: 10px;
   margin: 0 auto;
   /* margin-top: 110px; */
-  
 }
+
 .dag {
   /* margin: 20px; */
   border: 1px solid #ccc;
-  background: rgba(98, 117, 149, 0.5);
+  background: linear-gradient(to bottom, rgb(203, 239, 255, 0.3), rgb(60, 66, 133, 0.5));
   text-align: center;
-  padding: 10px;
   border-radius: 0 0 20px 20px;
+  overflow-wrap: break-word;
+  max-height: 240px;
+  overflow-y: scroll;
+}
+
+.dag p {
+  background: linear-gradient(to bottom, rgb(184, 182, 175), rgb(226, 221, 205), rgb(232, 229, 220), white);
+  background-clip: text;
+  color: transparent;
+}
+
+.heightChange {
+  height: 240px;
 }
 
 .dag h3 {
@@ -70,6 +112,7 @@ const unlockLuke = (dayObj: CalenderDay, dayNum: number) => {
   padding: 10px;
   margin: 0;
 }
+
 .dag button {
   margin: 10px 0;
   padding: 5px 10px;
@@ -79,14 +122,17 @@ const unlockLuke = (dayObj: CalenderDay, dayNum: number) => {
   border-radius: 30px;
   cursor: pointer;
 }
+
 .dag button:hover {
   filter: hue-rotate(150deg) saturate(0.5) brightness(1.1);
 }
-.dag iframe, .dag img {
-  padding: 4px 0;
-  height: auto;
-  max-width: 100%;
-  border-radius: 40px;
-}
 
+.dag iframe,
+.dag img {
+  padding: 4px 0;
+  height: 140px;
+  width: 120px;
+  border-radius: 20px;
+  object-fit: cover;
+}
 </style>
