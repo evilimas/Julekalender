be1fc7f48eb148562abba93f9a21518ec42f9001
FraChr <empynia@gmail.com>
2025-12-09 14:27:38 +0100

    randomized luker if not admin

--- src/stores/FirebaseStore.ts
+++ src/stores/FirebaseStore.ts
@@ -37,32 +37,7 @@ export type CalenderDay = {
   video?: string
 }
 
-type Calender = {
-  day1: CalenderDay
-  day2: CalenderDay
-  day3: CalenderDay
-  day4: CalenderDay
-  day5: CalenderDay
-  day6: CalenderDay
-  day7: CalenderDay
-  day8: CalenderDay
-  day9: CalenderDay
-  day10: CalenderDay
-  day11: CalenderDay
-  day12: CalenderDay
-  day13: CalenderDay
-  day14: CalenderDay
-  day15: CalenderDay
-  day16: CalenderDay
-  day17: CalenderDay
-  day18: CalenderDay
-  day19: CalenderDay
-  day20: CalenderDay
-  day21: CalenderDay
-  day22: CalenderDay
-  day23: CalenderDay
-  day24: CalenderDay
-}
+type Calender = Record<string, CalenderDay>
 
 type CalenderDocument = {
   id: string
@@ -83,32 +58,21 @@ type StyleDocument = {
   uid: string
 }
 
-const seedCalender = (): Calender => ({
-  day1: { day: 1, texts: 'Day 1 content', opened: false, openable: false },
-  day2: { day: 2, texts: 'Day 2 content', opened: false, openable: false },
-  day3: { day: 3, texts: 'Day 3 content', opened: false, openable: false },
-  day4: { day: 4, texts: 'Day 4 content', opened: false, openable: false },
-  day5: { day: 5, texts: 'Day 5 content', opened: false, openable: false },
-  day6: { day: 6, texts: 'Day 6 content', opened: false, openable: false },
-  day7: { day: 7, texts: 'Day 7 content', opened: false, openable: false },
-  day8: { day: 8, texts: 'Day 8 content', opened: false, openable: false },
-  day9: { day: 9, texts: 'Day 9 content', opened: false, openable: false },
-  day10: { day: 10, texts: 'Day 10 content', opened: false, openable: false },
-  day11: { day: 11, texts: 'Day 11 content', opened: false, openable: false },
-  day12: { day: 12, texts: 'Day 12 content', opened: false, openable: false },
-  day13: { day: 13, texts: 'Day 13 content', opened: false, openable: false },
-  day14: { day: 14, texts: 'Day 14 content', opened: false, openable: false },
-  day15: { day: 15, texts: 'Day 15 content', opened: false, openable: false },
-  day16: { day: 16, texts: 'Day 16 content', opened: false, openable: false },
-  day17: { day: 17, texts: 'Day 17 content', opened: false, openable: false },
-  day18: { day: 18, texts: 'Day 18 content', opened: false, openable: false },
-  day19: { day: 19, texts: 'Day 19 content', opened: false, openable: false },
-  day20: { day: 20, texts: 'Day 20 content', opened: false, openable: false },
-  day21: { day: 21, texts: 'Day 21 content', opened: false, openable: false },
-  day22: { day: 22, texts: 'Day 22 content', opened: false, openable: false },
-  day23: { day: 23, texts: 'Day 23 content', opened: false, openable: false },
-  day24: { day: 24, texts: 'Day 24 content', opened: false, openable: false },
-})
+const createCalendar = () => {
+  const totalDays = 24;
+  const startIndex = 0;
+  const calendar: Calender = {}
+
+  for (let i = startIndex; i < totalDays; i++) {
+    calendar[i] = {
+      day: i,
+      texts: `Day ${i} content`,
+      opened: false,
+      openable: false,
+    }
+  }
+  return calendar;
+}
 
 export const useFirebaseStore = defineStore('firebase', () => {
   const db = getFirestore()
@@ -130,11 +94,12 @@ export const useFirebaseStore = defineStore('firebase', () => {
       user.value = u
       // set getIdTokenResult(true) to force update token
       const idTokenResult = await u.getIdTokenResult(true)
+      isAdmin.value = idTokenResult.claims.admin === true
       await fetchMainJulekalender()
       await updateOpenableStatus(new Date().getDate())
       await fetchStyleDocument()
       // await createStyledocument()
-      isAdmin.value = idTokenResult.claims.admin === true
+
     } else {
       user.value = null
       isAdmin.value = false
@@ -201,7 +166,8 @@ export const useFirebaseStore = defineStore('firebase', () => {
   const createjulekalender = async (user: User): Promise<string> => {
     const calendarRef = await addDoc(collection(db, 'calenders'), {
       createdBy: { uid: user.uid, displayName: user.displayName },
-      calender: seedCalender(),
+      /*calender: seedCalender(),*/
+      calender: createCalendar(),
       calenderStarted: false,
       createdAt: serverTimestamp(),
       uid: user?.uid,
@@ -244,13 +210,23 @@ export const useFirebaseStore = defineStore('firebase', () => {
         const data = firstDoc.data() as CalenderDocument
         julekalender.value = data.calender
 
-        const sortedDays = Object.values(julekalender.value).sort((a, b) => a.day - b.day)
-        const sortedCalender: Calender = {} as Calender
-        sortedDays.forEach((day) => {
+
+        const days = Object.values(julekalender.value);
+        let finalDays: CalenderDay[];
+        if(isAdmin.value) {
+          finalDays = days.sort((a, b) => a.day - b.day);
+        }
+        else {
+          finalDays = days;
+        }
+
+        const cal: Calender = {} as Calender
+        finalDays.forEach((day) => {
           const key = `day${day.day}` as keyof Calender
-          sortedCalender[key] = day
+          cal[key] = day
         })
-        julekalender.value = sortedCalender
+        julekalender.value = cal
+
       }
     })
   }
